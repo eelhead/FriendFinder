2db1246961ea94a89fc90bf82aa26c359319c1bf
Adam MIller <adammiller@Adams-MBP.domain>
2018-02-13 16:54:01 -0700

    did scripts

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friendsArray = [{
+        name: "Clint Eastwood",
+        photo: "https://en.wikipedia.org/wiki/Clint_Eastwood#/media/File:Clint_Eastwood_-_1960s.JPG",
+        scores: [1, 3, 5, 3, 1, 1, 5, 4, 3, 2]
+    }, {
+        name: "Bruce Lee",
+        photo: "https://en.wikipedia.org/wiki/Bruce_Lee#/media/File:Bruce_Lee_1973.jpg",
+        scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+    },
+    {
+        name: "Chuck Norris",
+        photo: "https://en.wikipedia.org/wiki/Chuck_Norris#/media/File:Chuck_Norris,_The_Delta_Force_1986.jpg",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "Arnold Schwarzenegger",
+        photo: "https://en.wikipedia.org/wiki/Arnold_Schwarzenegger#/media/File:A._Schwarzenegger.jpg",
+        scores: [4, 1, 1, 2, 5, 4, 5, 2, 4, 2]
+    },
+    {
+        name: "Sylvester Stallone",
+        photo: "https://en.wikipedia.org/wiki/Sylvester_Stallone#/media/File:Sylvester-Stallone-2014-2.jpg",
+        scores: [3, 3, 3, 1, 1, 5, 5, 4, 5, 2]
+    },
+    {
+        name: "Jean-Claude Van Damme",
+        photo: "https://en.wikipedia.org/wiki/Jean-Claude_Van_Damme#/media/File:Jean-Claude_Van_Damme_2012.jpg",
+        scores: [2, 2, 3, 4, 5, 2, 1, 4, 4, 4]
+    },
+    {
+        name: "Steven Seagal",
+        photo: "https://en.wikipedia.org/wiki/Steven_Seagal#/media/File:Steven_Seagal_November_2016.jpg",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }
+];
+
+module.exports = friendsArray;
