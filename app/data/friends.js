1582992ee96796844ca2dd52a363071bd0e454ea
Adam MIller <adammiller@Adams-MBP.domain>
2018-02-19 17:13:55 -0700

    fixed jpeg links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,35 +1,35 @@
 var friendsArray = [{
         name: "Clint Eastwood",
-        photo: "https://en.wikipedia.org/wiki/Clint_Eastwood#/media/File:Clint_Eastwood_-_1960s.JPG",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/f/f3/Clint_Eastwood_-_1960s.JPG",
         scores: [1, 3, 5, 3, 1, 1, 5, 4, 3, 2]
     }, {
         name: "Bruce Lee",
-        photo: "https://en.wikipedia.org/wiki/Bruce_Lee#/media/File:Bruce_Lee_1973.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/c/ca/Bruce_Lee_1973.jpg",
         scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
     },
     {
         name: "Chuck Norris",
-        photo: "https://en.wikipedia.org/wiki/Chuck_Norris#/media/File:Chuck_Norris,_The_Delta_Force_1986.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chuck_Norris%2C_The_Delta_Force_1986.jpg",
         scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     },
     {
         name: "Arnold Schwarzenegger",
-        photo: "https://en.wikipedia.org/wiki/Arnold_Schwarzenegger#/media/File:A._Schwarzenegger.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/e/ed/Arnold_Schwarzenegger_2003.jpg",
         scores: [4, 1, 1, 2, 5, 4, 5, 2, 4, 2]
     },
     {
         name: "Sylvester Stallone",
-        photo: "https://en.wikipedia.org/wiki/Sylvester_Stallone#/media/File:Sylvester-Stallone-2014-2.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/3/32/Sylvester_Stallone_%281983%29.jpg",
         scores: [3, 3, 3, 1, 1, 5, 5, 4, 5, 2]
     },
     {
         name: "Jean-Claude Van Damme",
-        photo: "https://en.wikipedia.org/wiki/Jean-Claude_Van_Damme#/media/File:Jean-Claude_Van_Damme_2012.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/2/27/Jean-Claude_Van_Damme_2012.jpg",
         scores: [2, 2, 3, 4, 5, 2, 1, 4, 4, 4]
     },
     {
         name: "Steven Seagal",
-        photo: "https://en.wikipedia.org/wiki/Steven_Seagal#/media/File:Steven_Seagal_November_2016.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Steven_Seagal_November_2016.jpg",
         scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     }
 ];
